b47f6c7a09370f6495f9640ff27c213afcd65a38
Chris <christianbuchert@gmail.com>
2024-11-28 11:44:28 -0700

    Test refactor.

--- src/vector-clock.test.ts
+++ src/vector-clock.test.ts
@@ -2,48 +2,47 @@ import { describe, expect, it } from "vitest"
 import { VectorClock } from "./vector-clock"
 
 describe("VectorClock", () => {
-  const processId: string = "1"
+  const pid1: string = "1"
+  const pid2: string = "2"
 
   it("initializes the clock.", () => {
-    const clock = new VectorClock(processId, [])
+    const clock = new VectorClock(pid1, [])
 
-    expect(clock.getClock()).toEqual(new Map([[processId, 0]]))
+    expect(clock.getClock()).toEqual(new Map([[pid1, 0]]))
   })
 
   it("returns the value of the clock.", () => {
-    const clock = new VectorClock(processId, [])
+    const clock = new VectorClock(pid1, [])
 
-    expect(clock.getClock()).toEqual(new Map([[processId, 0]]))
+    expect(clock.getClock()).toEqual(new Map([[pid1, 0]]))
   })
 
   it("increments the clock.", () => {
-    const clock = new VectorClock(processId, [])
+    const clock = new VectorClock(pid1, [])
 
     clock.tick()
 
-    expect(clock.getClock()).toEqual(new Map([[processId, 1]]))
+    expect(clock.getClock()).toEqual(new Map([[pid1, 1]]))
   })
 
   it("sends the clock.", () => {
-    const clock = new VectorClock(processId, [])
+    const clock = new VectorClock(pid1, [])
 
-    expect(clock.send()).toEqual(new Map([[processId, 1]]))
+    expect(clock.send()).toEqual(new Map([[pid1, 1]]))
   })
 
   it("receives the clock.", () => {
-    const clock = new VectorClock(processId, [])
-    const receivedClock = new Map([[processId, 1]])
+    const clock = new VectorClock(pid1, [])
+    const receivedClock = new Map([[pid1, 1]])
 
     clock.receive(receivedClock)
 
-    expect(clock.getClock()).toEqual(new Map([[processId, 2]]))
+    expect(clock.getClock()).toEqual(new Map([[pid1, 2]]))
   })
 
   it("compares one clock ahead of another clock.", () => {
-    const pid1 = "1"
-    const pid2 = "2"
     const clock1 = new VectorClock(pid1, [pid2])
-    const clock2 = new VectorClock(pid1, [pid2])
+    const clock2 = new VectorClock(pid1, [pid1])
 
     clock1.tick()
 
@@ -51,10 +50,8 @@ describe("VectorClock", () => {
   })
 
   it("compares one clock behind another clock.", () => {
-    const pid1 = "1"
-    const pid2 = "2"
     const clock1 = new VectorClock(pid1, [pid2])
-    const clock2 = new VectorClock(pid1, [pid2])
+    const clock2 = new VectorClock(pid1, [pid1])
 
     clock2.tick()
 
@@ -62,19 +59,15 @@ describe("VectorClock", () => {
   })
 
   it("compares two equal clocks.", () => {
-    const pid1 = "1"
-    const pid2 = "2"
     const clock1 = new VectorClock(pid1, [pid2])
-    const clock2 = new VectorClock(pid1, [pid2])
+    const clock2 = new VectorClock(pid1, [pid1])
 
     expect(clock1.compare(clock2.getClock())).toBe(0)
   })
 
   it("compares two concurrent clocks.", () => {
-    const pid1 = "1"
-    const pid2 = "2"
     const clock1 = new VectorClock(pid1, [pid2])
-    const clock2 = new VectorClock(pid2, [pid2])
+    const clock2 = new VectorClock(pid2, [pid1])
 
     clock1.tick()
     clock2.tick()
